0950ee39b4f88b84cc10fdff2ee13cfc0622e086
f3l1p <felipe@steadyrent.com>
2024-03-06 00:02:19 +0100

    frameguard added

--- myApp.js
+++ myApp.js
@@ -1,60 +1,17 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0" }));
+app.use(helmet.frameguard({ action: "deny" }));
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
